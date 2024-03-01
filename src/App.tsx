6201fc460057b8f84792451140305ed356a5a117
g610g <gio.gonzales@carsu.edu.ph>
2024-03-02 00:41:06 +0800

    supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import "./index.css"
-import "./App.css"
-import Body from "./components/Body"
+import "./index.css";
+import "./App.css";
+import Body from "./components/Body";
+import { useState } from "react";
+import { supabase } from "./utils/supabase";
+import { definitions } from "../generated-types.ts";
 function App() {
+  const [countries, setCountries] = useState<any>([]);
+  async function getCountries() {
+    const { data } = await supabase
+      .from<definitions["countries"]>("countries")
+      .select("*");
+    setCountries(data);
+  }
   return (
     <div>
-      <Body/>
+      <Body />
     </div>
-      
-    )
+  );
 }
 
-export default App
+export default App;
